3091666d1dc11d02dabd4d1205819db02909b202
Bambi <Bambi@BambiMac.local>
2013-10-19 15:29:58 +0800

    Fix #15

---
+++ J15.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class J15 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double[] num = new double[10];
+		double avg = 0.0;
+		try {
+			while(sc.hasNextLine()){
+				String str = sc.nextLine();
+				String[] temp = str.split(" ");
+				if(temp.length != 10) {
+					break;
+				}
+				for(int i = 0; i < 10; i++) {
+					num[i] = Integer.parseInt(temp[i]);
+				}
+				Arrays.sort(num);
+				avg = (num[0]+num[9])/2;
+				System.out.println(avg);
+			}
+			
+		}
+		catch(Exception e) {
+				System.out.println("Error");
+		}
+	}
+}
